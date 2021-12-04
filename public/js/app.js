cf07529fcfb5ece9f803c6e770715a01e0cff5a3
Kaylee Spindler <gl13ch@Kaylees-MacBook-Air.local>
2021-12-03 20:34:22 -0600

    User auth added

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,17 @@
 const app = angular.module('MyApp', []);
+// const session = require('express-session');
 
 app.controller('MyController', ['$http', function($http){
     this.name = null;
     this.content = null;
     this.indexOfEditFormToShow = null;
+    this.loggedInUser = false;
     const controller = this;
 
     this.createMessage = function(){
         $http({
             method:'POST',
-            url: 'http://localhost:3000/messages/',
+            url: 'https://agile-temple-00865.herokuapp.com/messages/',
             data: {
                 name: this.name,
                 content: this.content
@@ -24,7 +26,7 @@ app.controller('MyController', ['$http', function($http){
     this.getMessages = function(){
         $http({
             method:'GET',
-            url: 'http://localhost:3000/messages/',
+            url: 'https://agile-temple-00865.herokuapp.com/messages/',
         }).then(function(response){
             controller.messages = response.data; //set value on success
         }, function(){
@@ -35,7 +37,7 @@ app.controller('MyController', ['$http', function($http){
     this.deleteMessage = function(message){
     $http({
         method:'DELETE',
-        url: 'http://localhost:3000/messages/' + message._id
+        url: 'https://agile-temple-00865.herokuapp.com/messages/' + message._id
     }).then(
         function(response){
             controller.getMessages();
@@ -45,13 +47,13 @@ app.controller('MyController', ['$http', function($http){
         }
     );
   }
+  this.getMessages(); //call immediately once controller is instantiated to test
 
   this.editMessage = function(message){
     $http({
         method:'PUT',
-        url: 'http://localhost:3000/messages/' + message._id,
+        url: 'https://agile-temple-00865.herokuapp.com/messages/' + message._id,
         data: {
-          // name: this.name,
           content: this.updatedContent
         }
     }).then(
@@ -64,7 +66,53 @@ app.controller('MyController', ['$http', function($http){
     );
   }
 
-    this.getMessages(); //call immediately once controller is instantiated to test
+  this.signup = function(){
+    $http({
+        url:'https://agile-temple-00865.herokuapp.com/users',
+        method:'POST',
+        data: {
+            username: this.signupUsername,
+            password: this.signupPassword
+        }
+    }).then(function(response){
+        controller.loggedInUser = response.data;
+    })
+  }
+
+  this.login = function(){
+  $http({
+      url:'https://agile-temple-00865.herokuapp.com/sessions',
+      method:'POST',
+      data: {
+          username: this.loginUsername,
+          password: this.loginPassword
+      }
+  }).then(function(response){
+      if(response.data.username){
+          controller.loggedInUser = response.data;
+      } else {
+          controller.loginUsername = null;
+          controller.loginPassword = null;
+      }
+    })
+  }
+
+  $http({
+      method:'GET',
+      url:'https://agile-temple-00865.herokuapp.com/sessions'
+  }).then(function(response){
+      console.log(response);
+  });
+
+  this.logout = function(){
+    $http({
+        url:'https://agile-temple-00865.herokuapp.com/sessions',
+        method:'DELETE'
+    }).then(function(){
+        controller.loggedInUser = false;
+    })
+  }
+
 
     }
   ]
