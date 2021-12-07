7d23b2c3c2543e7652ea2be72fda07e8d5bc1a4f
Kaylee Spindler <gl13ch@Kaylees-MacBook-Air.local>
2021-12-06 19:48:35 -0600

    messed with localstorage/sessionStorage

--- public/js/app.js
+++ public/js/app.js
@@ -1,33 +1,20 @@
-const app = angular.module('MyApp', ["pubnub.angular.service"]);
+'use strict';
 
-// app.factory('$localstorage', ['$window', function($window) {
-//   return {
-//     set: function(this.loggedInUser, true) {
-//       $window.localStorage[this.loggedInUser] = true;
-//     },
-//     get: function(this.loggedInUser, true) {
-//       return $window.localStorage[this.loggedInUser] || true || false;
-//     },
-//     setObject: function(this.loggedInUser, true) {
-//       $window.localStorage[this.loggedInUser] = JSON.stringify(true);
-//     },
-//     getObject: function(this.loggedInUser, true) {
-//       if($window.localStorage[this.loggedInUser] != undefined){
-//           return JSON.parse($window.localStorage[this.loggedInUser]);
-//       }else{
-//         return true || false;
-//       }
-//     },
-//     remove: function(this.loggedInUser){
-//       $window.localStorage.removeItem(this.loggedInUser);
-//     },
-//     clear: function(){
-//       $window.localStorage.clear();
-//     }
-//   }
+const app = angular.module('MyApp', []);
+
+// app.factory('storageService', ['$rootScope', function($rootScope) {
+//
+//     return {
+//         get: function(key) {
+//             return sessionStorage.getItem(key);
+//         },
+//         save: function(key, data) {
+//             sessionStorage.setItem(key, data);
+//         }
+//     };
 // }]);
 
-app.controller('MyController', ['$http', function($http){
+app.controller('MyController', ['$http', function($http, $scope){
     this.name = null;
     this.content = null;
     this.indexOfEditFormToShow = null;
@@ -115,10 +102,17 @@ app.controller('MyController', ['$http', function($http){
       }
   }).then(function(response){
       if(response.data.username){
-          controller.loggedInUser = response.data;
+        // console.log(response.data);
+
+        controller.loggedInUser = response.data;
+        // if (controller.loggedInUser = response.data) {
+        //   sessionStorage.setItem('data', JSON.stringify(response.data))
+        // }
+
+
       } else {
-          controller.loginUsername = null;
-          controller.loginPassword = null;
+        controller.loginUsername = null;
+        controller.loginPassword = null;
       }
     })
   }
@@ -128,6 +122,8 @@ app.controller('MyController', ['$http', function($http){
       url:'https://agile-temple-00865.herokuapp.com/sessions'
   }).then(function(response){
       console.log(response);
+      // let retrieved = sessionStorage.getItem('data')
+      // response.data = JSON.parse(retrieved);
   });
 
   this.logout = function(){
@@ -136,6 +132,11 @@ app.controller('MyController', ['$http', function($http){
         method:'DELETE'
     }).then(function(){
         controller.loggedInUser = false;
+        // if (controller.loggedInUser = false) {
+        //   sessionStorage.removeItem('data')
+        // }
+        // let retrieved = sessionStorage.getItem('data')
+        // response.data = JSON.parse(retrieved);
     })
   }
 
@@ -143,57 +144,86 @@ app.controller('MyController', ['$http', function($http){
 ]
 );
 
+//localStorage =============================================
+
+// app.factory('$localstorage', ['$window', function($window) {
+//   return {
+//     set: function(this.loggedInUser, true) {
+//       $window.localStorage[this.loggedInUser] = true;
+//     },
+//     get: function(this.loggedInUser, true) {
+//       return $window.localStorage[this.loggedInUser] || true || false;
+//     },
+//     setObject: function(this.loggedInUser, true) {
+//       $window.localStorage[this.loggedInUser] = JSON.stringify(true);
+//     },
+//     getObject: function(this.loggedInUser, true) {
+//       if($window.localStorage[this.loggedInUser] != undefined){
+//           return JSON.parse($window.localStorage[this.loggedInUser]);
+//       }else{
+//         return true || false;
+//       }
+//     },
+//     remove: function(this.loggedInUser){
+//       $window.localStorage.removeItem(this.loggedInUser);
+//     },
+//     clear: function(){
+//       $window.localStorage.clear();
+//     }
+//   }
+// }]);
+
 // LIVECHAT ===========================================
 
-app.controller('ChatCtrl', function($scope, Pubnub) {
-    $scope.channel = 'messages-channel';
-    // Generating a random uuid between 1 and 100 using an utility function from the lodash library.
-    $scope.uuid = Math.random(100).toString();
-    Pubnub.init({
-          publish_key: 'pub-c-d8776643-9fc5-489c-abd1-8b5429cddd50',
-          subscribe_key: 'sub-c-7b9d23c0-56d7-11ec-931a-1addb9510060',
-          uuid: $scope.uuid
-        });
-
-        // Send the messages over PubNub Network
-     $scope.sendMessage = function() {
-        // Don't send an empty message
-        if (!$scope.messageContent || $scope.messageContent === '') {
-             return;
-         }
-         Pubnub.publish({
-             channel: $scope.channel,
-             message: {
-                 content: $scope.messageContent,
-                 sender_uuid: $scope.uuid,
-                 date: new Date()
-             },
-             callback: function(m) {
-                 console.log(m);
-             }
-         });
-         // Reset the messageContent input
-         $scope.messageContent = '';
-
-     }
-
-     $scope.messages = [];
-
- // Subscribing to the ‘messages-channel’ and trigering the message callback
- Pubnub.subscribe({
-   channel: $scope.channel,
-   triggerEvents: ['callback']
- });
-
- // Listening to the callbacks
- $scope.$on(Pubnub.getMessageEventNameFor($scope.channel), function (ngEvent, m) {
-   $scope.$apply(function () {
-       $scope.messages.push(m)
-   });
- });
-
- // A function to display a nice uniq robot avatar
- $scope.avatarUrl = function(uuid){
-   return 'http://robohash.org/'+uuid+'?set=set2&bgset=bg2&size=70x70';
- };
-  });
+// app.controller('ChatCtrl', function($scope, Pubnub) {
+//     $scope.channel = 'messages-channel';
+//     // Generating a random uuid between 1 and 100 using an utility function from the lodash library.
+//     $scope.uuid = Math.random(100).toString();
+//     Pubnub.init({
+//           publish_key: 'pub-c-d8776643-9fc5-489c-abd1-8b5429cddd50',
+//           subscribe_key: 'sub-c-7b9d23c0-56d7-11ec-931a-1addb9510060',
+//           uuid: $scope.uuid
+//         });
+//
+//         // Send the messages over PubNub Network
+//      $scope.sendMessage = function() {
+//         // Don't send an empty message
+//         if (!$scope.messageContent || $scope.messageContent === '') {
+//              return;
+//          }
+//          Pubnub.publish({
+//              channel: $scope.channel,
+//              message: {
+//                  content: $scope.messageContent,
+//                  sender_uuid: $scope.uuid,
+//                  date: new Date()
+//              },
+//              callback: function(m) {
+//                  console.log(m);
+//              }
+//          });
+//          // Reset the messageContent input
+//          $scope.messageContent = '';
+//
+//      }
+//
+//      $scope.messages = [];
+//
+//  // Subscribing to the ‘messages-channel’ and trigering the message callback
+//  Pubnub.subscribe({
+//    channel: $scope.channel,
+//    triggerEvents: ['callback']
+//  });
+//
+//  // Listening to the callbacks
+//  $scope.$on(Pubnub.getMessageEventNameFor($scope.channel), function (ngEvent, m) {
+//    $scope.$apply(function () {
+//        $scope.messages.push(m)
+//    });
+//  });
+//
+//  // A function to display a nice uniq robot avatar
+//  $scope.avatarUrl = function(uuid){
+//    return 'http://robohash.org/'+uuid+'?set=set2&bgset=bg2&size=70x70';
+//  };
+//   });
